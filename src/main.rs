c6dd808f061dd0c24a3d92ed72d2f8e1022c87d4
Niklas von Moers <niklas.von.moers2@t-online.de>
2025-02-08 23:17:43 +0100

    refactor, support list indices in search term

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use serde_json::Value;
 
+use std::fs;
 use std::io::{self, BufReader, Read};
 
 fn read_from_stdin() -> Result<String, io::Error> {
@@ -81,73 +82,116 @@ fn search_json_value(
     expected_value: &str,
     current_path: Vec<String>,
 ) -> Vec<String> {
-    let mut results = Vec::new();
-
     match json_value {
-        Value::Object(obj) => {
-            let mut next_path = current_path.clone();
-            for (key, value) in obj {
-                next_path.push(key.clone());
-                results.extend(search_json_value(
-                    value,
-                    field_path_parts,
-                    field_name,
-                    expected_value,
-                    next_path.clone(),
-                ));
-                next_path.pop(); // Backtrack for next key
+        Value::Object(obj) => search_object(
+            obj,
+            field_path_parts,
+            field_name,
+            expected_value,
+            current_path,
+        ),
+        Value::Array(arr) => search_array(
+            arr,
+            field_path_parts,
+            field_name,
+            expected_value,
+            current_path,
+        ),
+        _ => Vec::new(), // For other types like String, Number, Bool, return empty results
+    }
+}
+
+fn search_object(
+    obj: &serde_json::Map<String, Value>,
+    field_path_parts: &[&str],
+    field_name: &str,
+    expected_value: &str,
+    current_path: Vec<String>,
+) -> Vec<String> {
+    let mut results = Vec::new();
+    let mut next_path = current_path.clone();
+
+    for (key, value) in obj {
+        next_path.push(key.clone());
+        results.extend(search_json_value(
+            value,
+            field_path_parts,
+            field_name,
+            expected_value,
+            next_path.clone(),
+        ));
+        next_path.pop(); // Backtrack for next key
+    }
+
+    results.extend(check_object_match(
+        obj,
+        field_path_parts,
+        field_name,
+        expected_value,
+        &current_path,
+    ));
+
+    results
+}
+
+fn check_object_match(
+    obj: &serde_json::Map<String, Value>,
+    field_path_parts: &[&str],
+    field_name: &str,
+    expected_value: &str,
+    current_path: &Vec<String>,
+) -> Vec<String> {
+    let mut results = Vec::new();
+    if !field_path_parts.is_empty() {
+        let mut current_path_index = 0;
+        let mut field_path_index = 0;
+
+        while current_path_index < current_path.len() && field_path_index < field_path_parts.len() {
+            if current_path[current_path_index] == field_path_parts[field_path_index] {
+                field_path_index += 1; // Move to next field path part if match is found
             }
+            current_path_index += 1; // Always move to next current path part
+        }
 
-            // Check for match at the current level
-            if !field_path_parts.is_empty() {
-                let path_len = field_path_parts.len();
-                let current_path_len = current_path.len();
-
-                if current_path_len >= path_len {
-                    // check if current path is long enough to potentially end with field_path
-                    let path_suffix: Vec<String> = current_path
-                        .iter()
-                        .skip(current_path_len - path_len)
-                        .cloned()
-                        .collect(); // Take suffix of current path
-                    if path_suffix
-                        .iter()
-                        .zip(field_path_parts.iter())
-                        .all(|(a, b)| a == *b)
-                    {
-                        // Check if current path *ends* with field_path
-                        if let Some(value) = obj.get(field_name) {
-                            if value_to_string(value).trim_matches('"') == expected_value {
-                                let full_path = current_path.join(".") + "." + field_name;
-                                results.push(full_path);
-                            }
-                        }
-                    }
-                }
-            } else {
-                // field_path_parts is empty, search anywhere logic
-                if let Some(value) = obj.get(field_name) {
-                    if value_to_string(value).trim_matches('"') == expected_value {
-                        let full_path = current_path.join(".") + "." + field_name;
-                        results.push(full_path);
-                    }
+        // Check if all parts of field_path_parts have been matched in the current_path
+        if field_path_index == field_path_parts.len() {
+            if let Some(value) = obj.get(field_name) {
+                if value_to_string(value).trim_matches('"') == expected_value {
+                    let full_path = current_path.join(".") + "." + field_name;
+                    results.push(full_path);
                 }
             }
         }
-        Value::Array(arr) => {
-            for (index, item) in arr.iter().enumerate() {
-                let mut next_path = current_path.clone();
-                next_path.push(index.to_string()); // Add array index to path
-                results.extend(search_json_value(
-                    item,
-                    field_path_parts,
-                    field_name,
-                    expected_value,
-                    next_path,
-                ));
+    } else {
+        // field_path_parts is empty, search anywhere logic
+        if let Some(value) = obj.get(field_name) {
+            if value_to_string(value).trim_matches('"') == expected_value {
+                let full_path = current_path.join(".") + "." + field_name;
+                results.push(full_path);
             }
         }
-        _ => {}
+    }
+    results
+}
+
+fn search_array(
+    arr: &Vec<Value>,
+    field_path_parts: &[&str],
+    field_name: &str,
+    expected_value: &str,
+    current_path: Vec<String>,
+) -> Vec<String> {
+    let mut results = Vec::new();
+    for (index, item) in arr.iter().enumerate() {
+        let mut next_path = current_path.clone();
+        next_path.push(index.to_string()); // Add array index to path
+        results.extend(search_json_value(
+            item,
+            field_path_parts,
+            field_name,
+            expected_value,
+            next_path,
+        ));
     }
     results
 }
@@ -162,63 +206,101 @@ fn value_to_string(value: &Value) -> String {
     }
 }
 
+fn process_json_input(
+    json_input_raw: String,
+    field_path_parts: &[&str],
+    field_name: &str,
+    expected_value: &str,
+) -> Vec<String> {
+    match serde_json::from_str(&json_input_raw) {
+        Ok(json_value) => search_json_value(
+            &json_value,
+            field_path_parts,
+            field_name,
+            expected_value,
+            Vec::new(), // Initial path is empty
+        ),
+        Err(e) => {
+            eprintln!("Error parsing JSON input: {}", e);
+            Vec::new() // Return empty results on parsing error, avoid program exit in processing files
+        }
+    }
+}
+
+fn process_file(
+    file_path: &str,
+    field_path_parts: &[&str],
+    field_name: &str,
+    expected_value: &str,
+) -> Vec<String> {
+    match fs::read_to_string(file_path) {
+        Ok(file_content) => {
+            let results =
+                process_json_input(file_content, field_path_parts, field_name, expected_value);
+            results
+        }
+        Err(e) => {
+            eprintln!("Error reading file '{}': {}", file_path, e);
+            Vec::new() // Return empty results on file reading error, continue with other files
+        }
+    }
+}
+
+fn handle_file_input(
+    json_files: &Vec<String>,
+    field_path_parts: &[&str],
+    field_name: &str,
+    expected_value: &str,
+) {
+    for file_path in json_files {
+        let search_results = process_file(file_path, field_path_parts, field_name, expected_value);
+        for result_path in search_results {
+            println!("{}", result_path);
+        }
+    }
+}
+
+fn handle_string_or_stdin_input(
+    json_string: &Option<String>,
+    field_path_parts: &[&str],
+    field_name: &str,
+    expected_value: &str,
+) {
+    let json_input_raw = match json_string {
+        Some(json_str) => json_str.clone(),
+        None => match read_from_stdin() {
+            Ok(stdin_json) => stdin_json,
+            Err(e) => {
+                eprintln!("Error reading from stdin: {}", e);
+                std::process::exit(1);
+            }
+        },
+    };
+
+    let search_results =
+        process_json_input(json_input_raw, field_path_parts, field_name, expected_value);
+    for result_path in search_results {
+        println!("{}", result_path);
+    }
+}
+
 fn main() {
     let args = Cli::parse();
     let search_term_raw = args.search_term;
     let json_files = args.json_files;
+    let json_string = args.json_string;
 
     match parse_search_term(&search_term_raw) {
         Ok((field_path_parts, field_name, expected_value)) => {
             if !json_files.is_empty() {
-                for file_path in json_files {
-                    match std::fs::read_to_string(&file_path) {
-                        Ok(file_content) => match serde_json::from_str(&file_content) {
-                            Ok(json_value) => {
-                                let search_results = search_json_value(
-                                    &json_value,
-                                    &field_path_parts,
-                                    field_name,
-                                    &expected_value,
-                                    Vec::new(), // Initial path is empty
-                                );
-                                for result_path in search_results {
-                                    println!("{}", result_path);
-                                }
-                            }
-                            Err(e) => {
-                                eprintln!("Error parsing JSON in file '{}': {}", file_path, e)
-                            }
-                        },
-                        Err(e) => eprintln!("Error reading file '{}': {}", file_path, e),
-                    }
-                }
+                handle_file_input(&json_files, &field_path_parts, field_name, &expected_value);
             } else {
-                let json_input_raw = match args.json_string {
-                    Some(json_str) => json_str,
-                    None => match read_from_stdin() {
-                        Ok(stdin_json) => stdin_json,
-                        Err(e) => {
-                            eprintln!("Error reading from stdin: {}", e);
-                            std::process::exit(1);
-                        }
-                    },
-                };
-
-                match serde_json::from_str(&json_input_raw) {
-                    Ok(json_value) => {
-                        let search_results = search_json_value(
-                            &json_value,
-                            &field_path_parts,
-                            field_name,
-                            &expected_value,
-                            Vec::new(), // Initial path is empty
-                        );
-                        for result_path in search_results {
-                            println!("{}", result_path);
-                        }
-                    }
-                    Err(e) => eprintln!("Error parsing JSON input: {}", e),
-                }
+                handle_string_or_stdin_input(
+                    &json_string,
+                    &field_path_parts,
+                    field_name,
+                    &expected_value,
+                );
             }
         }
         Err(e) => {
